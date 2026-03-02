81f9cee1e98a31713f07cb175ee98287e0f0ed38
DxRx99 <osher.zc@gmail.com>
2026-03-02 19:13:04 +0200

    Removed agent.py since the app isnt gonna be with ai

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ import sys
 from PyQt6.QtWidgets import QApplication, QToolTip
 from PyQt6.QtGui import QFont
 from src.ui import MainWindow
-from src.agent import AgentWorker
-from src.updater import UPM # Import the UPM
+from src.updater import UPM 
 
 def main():
     app = QApplication(sys.argv)
     
-    # --- UPM CHECK ---
-    # Check for updates silently on startup
+
     if UPM.check_for_updates():
-        print("Update available. In a real app, prompt user here.")
-        # UPM.apply_update() 
+        print("Update Available Please Update Nxyo")
 
     app.setStyle("Fusion")
     
@@ -22,10 +19,7 @@ def main():
     
     window = MainWindow()
     window.show()
-    
-    agent = AgentWorker()
-    agent.start()
-    
+
     sys.exit(app.exec())
 
 if __name__ == "__main__":
